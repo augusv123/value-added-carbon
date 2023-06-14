9a346510382e3ddda347bb8ad021e24627abf935
Mariano <mariano.b.fernandez@gmail.com>
2023-06-14 19:34:01 -0300

    simplify footer

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,20 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-footer',
     templateUrl: './footer.component.html',
     styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent {
     test : Date = new Date();
 
-    constructor(private router: Router ) {}
+    constructor() {}
 
-    ngOnInit() {
-
-    }
-    getPath(){
-      return this.router.url;
-    }
 }
